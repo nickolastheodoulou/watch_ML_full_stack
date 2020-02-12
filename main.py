f026055eb0f8d874096fd2440ef9ee878375e198
colin <nicktheodoulou>
2020-02-12 20:36:08 +0000

    - added price to scrape

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
 
+brand = 'rolex'
 
 browser = webdriver.Chrome()
-browser.get('https://www.chrono24.co.uk/rolex/index.htm?dosearch=true&query=rolex')
+browser.get('https://www.chrono24.co.uk/' + brand + '/index.htm?dosearch=true&query=' + brand)
 
 # time in milliseconds before each scroll
 list = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000]
@@ -18,9 +20,16 @@ html_rendered = browser.page_source
 browser.close()  # close the browser
 
 soup = BeautifulSoup(html_rendered, 'html.parser')  # create the soup
-mydivs = soup.findAll("div", {"class": "article-image-container"})  # find the class which contains the imaes
+img_divs = soup.findAll("div", {"class": "article-image-container"})  # find the class which contains the imaes
 
-for i in range(0, len(mydivs)):  # loop through the list
-    content = mydivs[i].img['src']  # extract the 'src' from the rendered page
+price_divs = soup.findAll("div", {"class": "article-price"})  # find the class with the prices
+
+for i in range(0, len(img_divs)):  # loop through the list
+    content = img_divs[i].img['src']  # extract the 'src' from the rendered page
+    print('brand = ' + brand)
     print(content)
 
+    price = price_divs[i].text
+    price = price.replace(',', '')
+    price = re.findall(r'\d+', price)
+    print(price[0])
