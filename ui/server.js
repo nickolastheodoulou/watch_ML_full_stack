f92d73d2cd69d1e51505d1fdbbd7c765d39238cc
colin <nicktheodoulou>
2020-02-17 19:53:31 +0000

    - removed unecessary files for prod - drop requirements remove build from gitignore

--- ui/server.js
+++ ui/server.js
@@ -5,10 +5,10 @@ app.set('port', (process.env.PORT || 3000))
 
 app.use(express.static(__dirname + '/build'))
 
-app.get('*', function(request, response){
-    response.sendFile(__dirname + '/build/index.html')
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/build/index.html')
 })
 
 app.listen(app.get('port'), function() {
-    console.log("Express server started on port", app.get('port'))
+  console.log("Express server started on port", app.get('port'))
 })
