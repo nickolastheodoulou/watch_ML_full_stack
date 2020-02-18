bae2f81909835506b02be8611e7fff03cf3c2c5e
colin <nicktheodoulou>
2020-02-18 09:41:03 +0000

    - add all files

---
+++ ui/build/precache-manifest.32eaf1abe4c71d2fffe3a0b18344814d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526bd674005b5b3d899cebf861c60c3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ff9c70dc51aed67df5e",
+    "url": "/static/css/main.718fe7b0.chunk.css"
+  },
+  {
+    "revision": "4ad338b17c452be5f195",
+    "url": "/static/js/2.6035ee79.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6035ee79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff9c70dc51aed67df5e",
+    "url": "/static/js/main.9cf4a568.chunk.js"
+  },
+  {
+    "revision": "9e19e0944bfc6625df6b",
+    "url": "/static/js/runtime-main.e0c44f74.js"
+  }
+]);
---
+++ ui/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+const API_ENDPOINT = "http://52.209.91.187/"
+const API_CLIENT = axios.create({
+      baseURL: API_ENDPOINT,
+      timeout: 10000
+    })
+
+class App extends React.Component {
+
+    state = {
+        predictions: {
+            brandPredictions: []
+        },
+        imgSrc: ""
+    } // initialise the imgSrc to blank rather tha null
+
+    _onDragOver(event) {event.preventDefault()}  // override the default evebns
+    _onDragLeave(event) {event.preventDefault()}
+
+    _onDrop(event) {
+        event. preventDefault()
+        var targetFile = event.dataTransfer.files[0]
+        var reader = new FileReader()
+        reader.readAsDataURL(targetFile)
+        // put the target image as the background once dropped
+        reader.onloadend = (event) => {this.setState({imgSrc: reader.result})}
+        var data = new FormData()
+        data.append('image', targetFile)
+        API_CLIENT.post('/classify', data, {headers: {"Content-Type": targetFile.type}})
+            .then((response) => {this.setState({predictions: response.data}) })
+            .catch((error) => {console.log(error)})  // post request to send the image
+    }
+
+    render() {
+
+        var ImagePreview
+
+
+        if(this.state.imgSrc)
+        {
+            ImagePreview = (<img src={this.state.imgSrc} alt ="image-of-a-watch" />)
+        }
+
+        var Predictions =[]
+        this.state.predictions.brandPredictions.forEach((item, index) => {
+            Predictions.push(
+                <p key = {`item-${index}`}>{item[0]} : {item[1]}</p>
+            )
+        })
+
+        return (
+            <div className="App">
+                <div className='file-dropzone'
+                onDragOver={(event) => {this._onDragOver(event)}}
+                onDragLeave={(event) =>{this._onDragLeave(event)}}
+                onDrop={(event) => {this._onDrop(event)}}>
+                    {ImagePreview}
+                </div>
+
+                <div    className='predictions'>
+                    {Predictions}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
